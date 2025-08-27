14b3824da0875ee7e361375a88cf2d969ba7d991
CigThePig <130598968+CigThePig@users.noreply.github.com>
2025-08-27 15:55:07 -0400

    Ignore node_modules

--- game.js
+++ game.js
@@ -44,7 +44,7 @@ import {
 import './ui.js';
 import { buildMap } from './map.js';
 import './enemies.js';
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 
 (() => {
   let tileSize = 1,
